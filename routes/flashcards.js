852a37a3e1c250349fb2a3c77eb2bb3ff1b6f8ad
Tom <tomwilliamfranklin@gmail.com>
2020-07-13 14:03:14 +0100

    updated dummy data to have multiple flashcard sets.

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -27,7 +27,74 @@ router.get('/', function(req, res, next) {
                 back: 'Rome',
                 flipped: false,
             },
-    ],
+        ],
+    },
+    {
+        title: 'Game Consoles',
+        description: 'Revision for game consoles', 
+        dateCreated: new Date(),
+        dateLastUpdated: new Date(),
+        public: true,
+        flashcards: [
+            {
+                front: 'First Microsoft Console',
+                back: 'Xbox',
+                flipped: false,
+            },
+            {
+                front: 'First ever commerical game console?',
+                back: 'Pong',
+                flipped: false,
+            },
+            {
+                front: 'First Nintendo Console?',
+                back: 'NES',
+                flipped: false,
+            },
+        ],
+    },
+    {
+        title: 'Types of Wood',
+        description: 'Revision for my tree exam', 
+        dateCreated: new Date(),
+        dateLastUpdated: new Date(),
+        public: true,
+        flashcards: [
+            {
+                front: 'some wood question, idk anything about wood',
+                back: 'Oak',
+                flipped: false,
+            },
+            {
+                front: 'some question about wood facts, once again, i know nothing',
+                back: 'Wood is used to make high quality benches',
+                flipped: false,
+            },
+        ],
+    },
+    {
+        title: 'European Empires',
+        description: 'Revision for my european history exam!!', 
+        dateCreated: new Date(),
+        dateLastUpdated: new Date(),
+        public: true,
+        flashcards: [
+            {
+                front: 'The sun never sets on the _',
+                back: 'British Empire',
+                flipped: false,
+            },
+            {
+                front: 'Viva La Revolution?',
+                back: 'Revolutionary France',
+                flipped: false,
+            },
+            {
+                front: 'The union of Austria, Hungary, Croatia and a few other places',
+                back: 'Austria-Hungary',
+                flipped: false,
+            },
+        ],
     }];
   
     res.send(flashcards);
